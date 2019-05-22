5d8ed597545f13a7fff0ef8b9c05aaa8c9fb1e3c
JianhangLin <linjianhang1010@gmail.com>
2019-05-22 14:55:55 +0800

    定义生成n个元素的随机数组的方法

--- src/main/java/work/jianhang/sort/selectionsort/SortTestHelper.java
+++ src/main/java/work/jianhang/sort/selectionsort/SortTestHelper.java
@@ -1,9 +1,30 @@
 package work.jianhang.sort.selectionsort;
 
+
+import com.sun.tools.javac.util.Assert;
+
+import java.util.Random;
+
 /**
  * 定义辅助函数
  */
 public class SortTestHelper {
 
+    /**
+     * 生成n个元素的随机数组，每个元素的随机范围为[rangeL, rangeR]
+     * @param n 数组个数
+     * @param rangeL 随机数组的最小值
+     * @param rangeR 随机数组的最大值
+     * @return int[]
+     */
+    public static int[] generateRandomArray(int n, int rangeL, int rangeR) {
+        Assert.check(rangeL < rangeR);
+        int[] arr = new int[n];
+        Random rand = new Random();
+        for (int i=0; i<n; i++) {
+            arr[i] = rand.nextInt(rangeR) + rangeL;
+        }
+        return arr;
+    }
 
 }
