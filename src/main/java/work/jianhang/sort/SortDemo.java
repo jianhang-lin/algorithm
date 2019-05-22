ecdf6b0b41d1ffa8040a93c0e5c978a38e00feb9
JianhangLin <linjianhang1010@gmail.com>
2019-05-22 22:38:24 +0800

    更新

--- src/main/java/work/jianhang/sort/SortDemo.java
+++ src/main/java/work/jianhang/sort/SortDemo.java
@@ -8,7 +8,8 @@ public class SortDemo {
     public static void main(String[] args) {
         int n = 10000;
         int[] arr = SortTestHelper.generateRandomArray(n, 0, n);
-        SelectionSort.sort(arr, n);
+        SelectionSort selectionSort = new SelectionSort();
+        selectionSort.sort(arr, n);
         Integer[] arrInteger = SortTestHelper.int2Integer(arr, n);
         SortTestHelper.printArray(arrInteger, n);
 
