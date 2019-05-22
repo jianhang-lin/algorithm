12323c5a99678a8ec99db6eddb33a376f989fb76
JianhangLin <linjianhang1010@gmail.com>
2019-05-22 17:51:13 +0800

    增加把int类型的数组转换成Intege类型的数组的方法

--- src/main/java/work/jianhang/sort/SortTestHelper.java
+++ src/main/java/work/jianhang/sort/SortTestHelper.java
@@ -1,4 +1,4 @@
-package work.jianhang.sort.selectionsort;
+package work.jianhang.sort;
 
 import org.junit.Assert;
 
@@ -7,7 +7,7 @@ import java.util.Random;
 /**
  * 定义辅助函数
  */
-public class SortTestHelper {
+class SortTestHelper {
 
     /**
      * 生成n个元素的随机数组，每个元素的随机范围为[rangeL, rangeR]
@@ -16,9 +16,9 @@ public class SortTestHelper {
      * @param rangeR 随机数组的最大值
      * @return Integer[]
      */
-    public static Integer[] generateRandomArray(int n, int rangeL, int rangeR) {
+    static int[] generateRandomArray(int n, int rangeL, int rangeR) {
         Assert.assertTrue(rangeL <= rangeR);
-        Integer[] arr = new Integer[n];
+        int[] arr = new int[n];
         Random rand = new Random();
         for (int i=0; i<n; i++) {
             arr[i] = rand.nextInt(rangeR) + rangeL;
@@ -27,12 +27,26 @@ public class SortTestHelper {
     }
 
     /**
+     * 把int类型的数组转换成Intege类型的数组
+     * @param arr int类型的数组
+     * @param n 数组的个数
+     * @return Intege类型的数组
+     */
+    static Integer[] int2Integer(int[] arr, int n) {
+        Integer[] integers = new Integer[n];
+        for (int i=0; i<n; i++) {
+            integers[i] = arr[i];
+        }
+        return integers;
+    }
+
+    /**
      * 打印出随机数组的数据
      * @param arr 随机数组
      * @param n 数组的个数
      * @param <T> 泛型
      */
-    public static <T> void printArray(T[] arr, int n) {
+    static <T> void printArray(T[] arr, int n) {
         StringBuilder sb = new StringBuilder();
         for (int i=0; i<n; i++) {
             sb.append(arr[i]).append(" ");
