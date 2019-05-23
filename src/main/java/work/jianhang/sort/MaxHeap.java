ff7d7af7b8ebae0e7277d5d70d84b7c363d50a36
JianhangLin <linjianhang1010@gmail.com>
2019-05-23 15:45:31 +0800

    更新最大堆实体类

--- src/main/java/work/jianhang/sort/MaxHeap.java
+++ src/main/java/work/jianhang/sort/MaxHeap.java
@@ -3,9 +3,9 @@ package work.jianhang.sort;
 import org.junit.Assert;
 
 /**
- * 最大堆
+ * 最大堆实体类
  */
-public class MaxHeap<E> {
+public class MaxHeap {
     private int[] data;
     private int count;
     private int capacity;
@@ -38,10 +38,36 @@ public class MaxHeap<E> {
         shiftUp(count);
     }
 
+    public int extractMax() {
+        Assert.assertTrue(count > 0);
+        int ret = data[1];
+        swap(data, 1, count);
+        count--;
+        shiftDown(1);
+        return ret;
+    }
+
     private void shiftUp(int k) {
         while (k > 1 && data[k/2] < data[k]) {
             swap(data, k/2, k);
-            k = k/2;
+            k /= 2;
+        }
+    }
+
+    private void shiftDown(int k) {
+        // 有子节点
+        while (2*k <= count) {
+            int j = 2*k;// 在此轮循环中，data[k]和data[j]交换位置
+            // 是否存在右子节点，并且右子节点大于左子节点
+            if (j +1 <= count && data[j+1] > data[j]) {
+                j = j+1;
+            }
+            // 父节点的数据是否大于子节点的数据
+            if (data[k] >= data[j]) {
+                break;
+            }
+            swap(data, k, j);
+            k=j;
         }
     }
 
@@ -75,4 +101,5 @@ public class MaxHeap<E> {
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
+
 }
